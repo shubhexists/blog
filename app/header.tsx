01b7eba456558b00f9558e2a9d437c4839a29bde
shubhexists <shubh622005@gmail.com>
2024-11-19 23:08:47 +0530

    feat: added github to headers

--- app/header.tsx
+++ app/header.tsx
@@ -1,6 +1,5 @@
 import { ThemeToggle } from "./theme-toggle";
 import { Logo } from "./logo";
-import Link from "next/link";
 
 export function Header() {
   return (
@@ -9,8 +8,14 @@ export function Header() {
 
       <nav className="font-mono text-xs grow justify-end items-center flex gap-1 md:gap-3">
         <ThemeToggle />
-
-      
+        <a
+          href="https://x.com/shubh_exists"
+          target="_blank"
+          className="inline-flex hover:bg-gray-200 dark:hover:bg-[#313131] active:bg-gray-300 dark:active:bg-[#242424] items-center p-2 rounded-sm transition-[background-color] whitespace-nowrap -mr-2"
+        >
+          <GithubIcon style={{ marginRight: 4 }} />
+          <span className="hidden md:inline">Github </span>
+        </a>
         <a
           href="https://x.com/shubh_exists"
           target="_blank"
@@ -39,6 +44,23 @@ function TweetIcon(props: any) {
         stroke="none"
         strokeWidth={1}
         d="M8.28 20.26c7.55 0 11.68-6.26 11.68-11.67v-.53c.8-.58 1.49-1.3 2.04-2.13-.74.33-1.53.54-2.36.65.85-.5 1.5-1.32 1.8-2.28-.78.48-1.66.81-2.6 1a4.1 4.1 0 0 0-7 3.74c-3.4-.17-6.43-1.8-8.46-4.29a4.1 4.1 0 0 0 1.28 5.48c-.68-.02-1.3-.2-1.86-.5v.05a4.11 4.11 0 0 0 3.29 4.02 4 4 0 0 1-1.85.08 4.1 4.1 0 0 0 3.83 2.85A8.23 8.23 0 0 1 2 18.43a11.67 11.67 0 0 0 6.28 1.83"
+      />
+    </svg>
+  );
+}
+
+function GithubIcon(props: any) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={16}
+      height={16}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.17 6.84 9.5.5.09.68-.22.68-.48 0-.24-.01-.87-.01-1.7-2.78.6-3.37-1.34-3.37-1.34-.46-1.17-1.12-1.48-1.12-1.48-.91-.62.07-.61.07-.61 1 .07 1.52 1.03 1.52 1.03.9 1.53 2.36 1.09 2.94.83.09-.65.35-1.09.63-1.34-2.22-.25-4.56-1.11-4.56-4.92 0-1.09.39-1.98 1.02-2.68-.1-.25-.44-1.27.1-2.65 0 0 .83-.27 2.73 1.02A9.45 9.45 0 0 1 12 6.85c.85.004 1.7.11 2.5.33 1.9-1.3 2.73-1.02 2.73-1.02.54 1.38.2 2.4.1 2.65.63.7 1.02 1.59 1.02 2.68 0 3.81-2.35 4.67-4.58 4.91.36.31.68.93.68 1.87 0 1.35-.01 2.43-.01 2.76 0 .26.18.58.69.48A10.02 10.02 0 0 0 22 12c0-5.52-4.48-10-10-10z"
       />
     </svg>
   );
