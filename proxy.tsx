537d96b6698984a1c9bdf77d84db36bd3ee6f5ea
shubhexists <shubh622005@gmail.com>
2026-07-05 23:41:54 +0530

    fix: upgrade nextjs version

--- proxy.tsx
+++ proxy.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 let initialDate = Date.now();
 
-export default async function middleware() {
+export function proxy() {
   const next = NextResponse.next();
   next.headers.set("x-edge-age", String(Date.now() - initialDate));
   return next;
